9c7a91bf7ed671e59f3c3a12c1c19c3a12c0ff69
s32429 <s32429@pjwstk.edu.pl>
2026-03-30 13:04:13 +0200

    Utworzenie klasy Presentation_Remote

---
+++ DOMAIN/Presentation_Remote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s32429_rent_shop.DOMAIN
+{
+    public enum Presentation_Remote
+    {
+        USB_A,
+        USB_C,
+        Bluetooth
+    }
+
+    public class Prezentation_Remote : Equipment
+    {
+        private Presentation_Remote Interface { get; set; }
+
+        public Prezentation_Remote(string Vendor, string Model, string Serial_Number, Presentation_Remote Interface) : base(Vendor, Model, Serial_Number)
+        {
+            this.Interface = Interface;
+        }
+    }
+}
