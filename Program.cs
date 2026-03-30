991c82adb4329db7553fc4eba4432189939c4998
s32429 <s32429@pjwstk.edu.pl>
2026-03-30 14:52:49 +0200

    Dodanie przykładowych użytkowników i sprzętu do systemu

--- Program.cs
+++ Program.cs
@@ -1,37 +1,44 @@
-﻿using System.Collections.Generic;
-using s32429_rent_shop.DOMAIN;
+﻿using s32429_rent_shop.DOMAIN;
+using s32429_rent_shop.SERVICE;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        var laptop = new Laptop(
+        var service = new RentService();
+
+        // Przykładowe dodanie sprzętu do systemu
+        service.AddEquipment(new Laptop(
             "Dell",
             "XPS 15",
             "JSI89S0",
             8192,
             "Intel Core i7-8900K",
             16
-            );
+        ));
 
-        var projector = new Projector(
+        service.AddEquipment(new Projector(
             "Epson",
             "EB-U05",
             "PRJ12345",
             "1920x1080",
             3200,
             new List<Projector_Interface> { Projector_Interface.HDMI, Projector_Interface.VGA }
-        );
+        ));
 
-        var camera = new Camera(
+        service.AddEquipment(new Camera(
             "Canon",
             "EOS 250D",
             "CAM98765",
             24,
             Camera_Interface.USB_C
-        );
+        ));
 
-        var student = new Student("Jan", "Kowalski", "85010212345");
-        var employee = new Employee("Anna", "Nowak", "90030354321");
+        // Przykładowe dodanie użytkowników do systemu
+        service.AddUser(new Student("Jan", "Kowalski", "85010212345"));
+        service.AddUser(new Employee("Anna", "Nowak", "90030354321"));
+
+
     }
 }
